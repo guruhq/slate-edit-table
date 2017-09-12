e43542475fdc805ee8812d82c57ded8663c12156
Ryan Yurkanin <ryurkanin@getguru.com>
2017-09-12 11:20:31 -0400

    onEnter bug

--- dist/onEnter.js
+++ dist/onEnter.js
@@ -6,10 +6,9 @@ var insertRow = require('./changes/insertRow');
  * Go to the row below on Enter if possible, if not, create a new row.
  */
 function onEnter(event, data, change, opts) {
-    event.preventDefault();
-    var transform = state.transform();
+  event.preventDefault();
 
-    return insertRow(opts, change);
+  return insertRow(opts, change);
 }
 
 module.exports = onEnter;
