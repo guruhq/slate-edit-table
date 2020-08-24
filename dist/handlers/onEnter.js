ecee274f27720b446f6908aa51ee8bd54b716c43
Ivana Veliskova <iveliskova@getguru.com>
2020-08-24 09:54:13 -0400

    Clean up

--- dist/handlers/onEnter.js
+++ dist/handlers/onEnter.js
@@ -12,6 +12,7 @@ var _changes = require('../changes');
  * Insert a new row when pressing "Enter"
  */
 function onEnter(event, change, editor, opts) {
+    // This is a custom change to have the enter key move to the next row instead of creating a new one
     event.preventDefault();
     var value = change.value;
 
@@ -29,28 +30,10 @@ function onEnter(event, change, editor, opts) {
     // Move back to initial cell (insertRow moves selection automatically).
     change.select(selection);
 
-    // Move
-    // moveSelectionBy(opts, change, 0, 1);
-
+    // Move by 0 horizontally and 1 vertically
     (0, _changes.moveSelectionBy)(opts, change, 0, 1);
 
     return change;
-
-    //   const { selection, document } = change.value;
-    //   const pos = TablePosition.create(opts, document, selection.startKey);
-
-    //   if (
-    //     !selection.hasFocusAtStartOf(pos.cell) &&
-    //     !selection.hasFocusAtEndOf(pos.cell)
-    //   ) {
-    //     return undefined;
-    //   }
-
-    //   if (event.shiftKey) {
-    //     return change.splitBlock().setBlocks({ type: opts.typeContent, data: {} });
-    //   }
-
-    //   return insertRow(opts, change);
 }
 
 exports.default = onEnter;
