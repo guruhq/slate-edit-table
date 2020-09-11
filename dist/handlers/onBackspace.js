bff3fa3c80b30243ac4ba57c593aa415b8963b74
Ivana Veliskova <iveliskova@getguru.com>
2020-09-11 11:14:44 -0400

    Updating deleting to b fixed

--- dist/handlers/onBackspace.js
+++ dist/handlers/onBackspace.js
@@ -1,81 +1,51 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 
-require("slate");
+var _slate = require('slate');
 
-var _changes = require("../changes");
+var _changes = require('../changes');
 
 function onBackspace(event, change, editor, opts) {
-  var value = change.value;
-  var startBlock = value.startBlock,
-      endBlock = value.endBlock,
-      selection = value.selection,
-      document = value.document;
+    var value = change.value;
+    var startBlock = value.startBlock,
+        startOffset = value.startOffset,
+        isCollapsed = value.isCollapsed,
+        endBlock = value.endBlock;
 
+    // If a cursor is collapsed at the start of the block, do nothing
 
-  var startCell = document.getClosest(startBlock.key, opts.isCell);
-  var endCell = document.getClosest(endBlock.key, opts.isCell);
-
-  var startBlockIndex = startCell.nodes.findIndex(function (block) {
-    return block.key == startBlock.key;
-  });
-
-  // If a cursor is collapsed at the start of the first block, do nothing
-  if (startBlockIndex === 0 && selection.isAtStartOf(startBlock)) {
-    if (startBlock.isVoid) {
-      // Delete the block normally if it is a void block
-      return undefined;
+    if (startOffset === 0 && isCollapsed) {
+        event.preventDefault();
+        return change;
     }
 
+    // If "normal" deletion, we continue
+    if (startBlock === endBlock) {
+        return null;
+    }
+
+    // If cursor is between multiple blocks,
+    // we clear the content of the cells
     event.preventDefault();
-    return change;
-  }
 
-  // If "normal" deletion, we continue
-  if (startCell === endCell) {
-    return undefined;
-  }
+    var blocks = value.blocks;
+    var focusBlock = value.focusBlock;
 
-  // If cursor is between multiple blocks,
-  // we clear the content of the cells.
-  event.preventDefault();
+    blocks.forEach(function (block) {
+        if (block.type !== opts.typeCell) {
+            return change;
+        }
 
-  var blocks = value.blocks;
+        var cellRange = _slate.Range.create().moveToRangeOf(block);
 
-  // Get all cells that contains the selection
-
-  var cells = blocks.map(function (node) {
-    return node.type === opts.typeCell ? node : document.getClosest(node.key, function (a) {
-      return a.type === opts.typeCell;
+        return change.deleteAtRange(cellRange);
     });
-  }).toSet();
 
-  // If the cursor is at the very end of the first cell, ignore it.
-  // If the cursor is at the very start of the last cell, ignore it.
-  // This behavior is to compensate hanging selection behaviors:
-  // https://github.com/ianstormtaylor/slate/pull/1605
-  var ignoreFirstCell = value.selection.collapseToStart().isAtEndOf(cells.first());
-  var ignoreLastCell = value.selection.collapseToEnd().isAtStartOf(cells.last());
-
-  var cellsToClear = cells;
-  if (ignoreFirstCell) {
-    cellsToClear = cellsToClear.rest();
-  }
-  if (ignoreLastCell) {
-    cellsToClear = cellsToClear.butLast();
-  }
-
-  // Clear all the selection
-  cellsToClear.forEach(function (cell) {
-    return (0, _changes.clearCell)(opts, change, cell);
-  });
-
-  // Update the selection properly, and avoid reset of selection
-  var updatedStartCell = change.value.document.getDescendant(cellsToClear.first().key);
-  return change.collapseToStartOf(updatedStartCell);
+    // Clear selected cells
+    return change.collapseToStartOf(focusBlock);
 }
 
 exports.default = onBackspace;
